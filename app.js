75170cbe6258223fa14791df441a96af8b135fa2
davieu <davisumana@gmail.com>
2018-09-07 18:41:28 -0400

    game variable and selecting

--- app.js
+++ app.js
@@ -7,11 +7,25 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayers, dice;
+var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayers = 0;
+activePlayer = 0;
 
+//roll a dice randomly 1-6
 dice = Math.floor(Math.random()*6)+1;
-console.log(dice);
+
+//select the activePlayer and gives current dice score to DOM using type-coercion. ('#current-' + activePlayer)
+//.textContent can only set plain text and no HTML.
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//.innerHTML you can add html. html needs to be in string for javascript to parse it.
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//gets value of #score-1.
+var x = document.querySelector('#score-1').textContent;
+console.log(x);
+
+//hides dice at the beginning of game.
+document.querySelector('.dice').style.display = 'none';
